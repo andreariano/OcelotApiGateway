86b6e16414a319097bce640478a1e7e1740f0a04
Andre Luiz de Carvalho Ariano <andre.ariano@programmers.com.br>
2020-01-10 09:00:13 -0300

    Sample authentication API Gateway with ocelot

---
+++ api-gateway/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Ocelot.DependencyInjection;
+
+namespace Gateway.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddOcelot($"Routes/{hostingContext.HostingEnvironment.EnvironmentName}", hostingContext.HostingEnvironment);
+                })
+                .UseStartup<Startup>();
+    }
+}
---
+++ api-gateway/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
+
+namespace Gateway.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication()
+                .AddJwtBearer("AzureAD", options =>
+                {
+                    options.Audience = Configuration.GetValue<string>("AzureAd:Audience");
+                    options.Authority = Configuration.GetValue<string>("AzureAd:Instance") + Configuration.GetValue<string>("AzureAd:TenantId");
+
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = Configuration.GetValue<string>("AzureAd:Issuer"),
+                        ValidAudience = Configuration.GetValue<string>("AzureAd:Audience")
+                    };
+                }
+            );
+
+            services.AddOcelot(Configuration);
+            services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(365);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseHsts();
+
+            app.UseAuthentication();
+            app.UseOcelot();
+        }
+    }
+}
